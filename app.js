afe2f4c4da75b062b5e722d81069f88ca785d5e7
robin <lb.robin1991@gmail.com>
2016-08-24 18:09:08 +0800

    add koa-error  方便调试

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const Koa = require('koa')
 const app = new Koa();
+
+// 输出错误日志
 const logger = require('./config/logger')
-// console.log('logger', logger);
+
+// 实时显示错误在命令行和网页
+const onerror = require('koa-onerror')
+onerror(app);
+
 app.use(async (ctx, next) => {
+  throw new Error('123')
   console.time(`${ctx.method} ${ctx.url}`);
   await next();
   console.timeEnd(`${ctx.method} ${ctx.url}`);
