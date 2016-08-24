33d36b106852f53aa6e0e2456a8eeaae68cbfd25
robin <lb.robin1991@gmail.com>
2016-08-24 17:30:33 +0800

    运行koa2安装最小量

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-import Koa from 'koa'
-const app = new Koa();
-
-
-// app.use(async (ctx, next) => {
-//   const start = new Date();
-//   await next();
-//   const ms = new Date() - start;
-//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
-// });
-// response
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+// set babel in entry file
+require('babel-register')({
+  plugins: ['transform-async-to-generator']
 });
 
-app.listen(3000);
+require('./app.js');
