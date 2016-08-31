56ff08c110602f3a279f4369c0296d6987a16224
robin <lb.robin1991@gmail.com>
2016-08-31 10:17:41 +0800

    添加路由模块，并独立出来

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Koa = require('koa')
 const app = new Koa();
-
+import router from './routers'
 // require('./gb-schema.js')
 // 输出错误日志
 const logger = require('./config/logger')
@@ -14,16 +14,21 @@ app.use(async (ctx, next) => {
   await next();
   console.timeEnd(`${ctx.method} ${ctx.url}`);
 });
+//
+// // 路由
+// const router = require('koa-router')();
 
-// 路由
-const router = require('koa-router')();
+/**
+ * 注入路由信息
+ */
+router(app);
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'hello koa2';
-});
-
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
+// router.get('/', function (ctx, next) {
+//   ctx.body = 'hello koa2';
+// });
+//
+// app
+//   .use(router.routes())
+//   .use(router.allowedMethods());
 
 app.listen(3000);
